d4fd3195f7903909f7bb311c59ec6bbd4bf21e23
Hisanori Shimakawa <shimakawa.hisanori@gmail.com>
2024-06-01 14:28:53 +0900

    use session storage

---
+++ go.mod
@@ -0,0 +1,7 @@
+module hisanori-long/go-web
+
+go 1.22.2
+
+require github.com/gorilla/sessions v1.2.2
+
+require github.com/gorilla/securecookie v1.1.2 // indirect
--- web.go
+++ web.go
@@ -4,6 +4,8 @@ import (
 	"log"
 	"net/http"
 	"text/template"
+
+	"github.com/gorilla/sessions"
 )
 
 // Temps is a struct that holds all the templates.
@@ -49,20 +51,37 @@ func index(w http.ResponseWriter, rq *http.Request, tmp *template.Template) {
 	}
 }
 
+var cs *sessions.CookieStore = sessions.NewCookieStore([]byte("secret-key-12345"))
+
 // hello handler
 func hello(w http.ResponseWriter, rq *http.Request, tmp *template.Template) {
-	msg := "type name and password:"
+	msg := "login name & password:"
+
+	ses, _ := cs.Get(rq, "hello-session")
+
 	if rq.Method == "POST" {
+		ses.Values["login"] = nil
+		ses.Values["name"] = nil
 		nm := rq.PostFormValue("name")
-		pw := rq.PostFormValue("password")
-		msg = "name: " + nm + ", password: " + pw
+		pw := rq.PostFormValue("pass")
+		if nm == pw {
+			ses.Values["login"] = true
+			ses.Values["name"] = nm
+		}
+		ses.Save(rq, w)
+	}
+
+	flg, _ := ses.Values["login"].(bool)
+	lname, _ := ses.Values["name"].(string)
+	if flg {
+		msg = "logined: " + lname
 	}
 
 	item := struct {
 		Title   string
 		Message string
 	}{
-		Title:   "Send Values",
+		Title:   "Session",
 		Message: msg,
 	}
 
